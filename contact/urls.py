8d959e03ea81588e746ccd3affbc70102d810ecb
Mayzon <mayzon.santos@gmail.com>
2025-03-23 21:44:37 -0300

    Segue processo de configuração

--- contact/urls.py
+++ contact/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
+
 from contact import views
 
 
 app_name = 'contact'
 
 urlpatterns = [
+    path('search/', views.search, name='search'),
     path('', views.index, name='index'),
+
+    # Contato (CRUD)
+    path('contact/<int:contact_id>/', views.contact, name='contact'),
 ]
